330a7abaac5fcfa07857eae64f07e35dfe2c6b12
thierrykith <thierry.kith@luxpmsoft.com>
2021-12-16 10:58:41 +0100

    Add VAT verification

--- src/components-site/views/authentication/signup/Company.js
+++ src/components-site/views/authentication/signup/Company.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import validate from 'validate-vat';
+
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -22,8 +24,22 @@ const Company = (props) => {
   const company = useSelector(state => state.signup.company);
   const billingAddress = useSelector(state => state.signup.billingAddress);
   const vat = useSelector(state => state.signup.vat);
-  // Boolean to disable or enable confirm button if T&C are checked or not
+
+  // Inputs error
+  const [vatError, setVatError] = useState('');
+  // Boolean to know if T&C are checked or not
   const [tc, setTc] = useState(false);
+  // Boolean to disable or enable confirm button
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  /**
+   * Validate all inputs of this page
+   */
+  const validateInputs = (vat, tc) => {
+    const vatValidation = vat === '' || (vat !== '' && vat.length < 16);
+    setVatError(vatValidation ? '' : 'Please check the VAT number format. It must be less than 16 characters.');
+    setCanSubmit(vatValidation && tc);
+  }
 
   const onChangeValue = (inputName, value) => {
     switch (inputName) {
@@ -35,7 +51,11 @@ const Company = (props) => {
         break;
       case 'vat':
         dispatch(setVat(value));
+        validateInputs(value, tc);
         break;
+      case 'tc':
+        setTc(value);
+        validateInputs(vat, value);
       default:
         break;
     }
@@ -55,9 +75,9 @@ const Company = (props) => {
       </Typography>
 
       <InputLabel sx={{ mt: 4 }}>Company name</InputLabel>
-      <CustomInput 
-        placeholder="Type your company name" 
-        fullWidth  
+      <CustomInput
+        placeholder="Type your company name"
+        fullWidth
         id="company"
         name="company"
         value={company}
@@ -65,9 +85,9 @@ const Company = (props) => {
       />
 
       <InputLabel sx={{ mt: 2 }}>Billing address</InputLabel>
-      <CustomInput 
-        placeholder="Type your billing address" 
-        fullWidth  
+      <CustomInput
+        placeholder="Type your billing address"
+        fullWidth
         id="billing-address"
         name="billing-address"
         value={billingAddress}
@@ -75,24 +95,27 @@ const Company = (props) => {
       />
       
       <InputLabel sx={{ mt: 2 }}>Vat number</InputLabel>
-      <CustomInput 
-        placeholder="Type your vat number (eg - GB2803414523)" 
-        fullWidth  
+      <CustomInput
+        placeholder="Type your vat number (eg - GB2803414523)"
+        fullWidth
         id="vat"
         name="vat"
         value={vat}
         onChange={(event) => onChangeValue('vat', event.target.value)}
       />
+      <Typography variant="caption" color="error">
+        {vatError}
+      </Typography>
         
       <Typography variant="body2" mt={4}>
         Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem 
         Lorem IpsumLorem IpsumLorem IpsumIpsumLorem IpsumLorem Ipsum
       </Typography>
 
-      <FormControlLabel 
+      <FormControlLabel
         sx={{ color: "#9a9a9a" }}
-        control={<Checkbox value={tc} onChange={(event) => setTc(!tc)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30 }, mr: '5px', color: '#fff' }} />} 
-        label="Agree to T&C" 
+        control={<Checkbox value={tc} onChange={(event) => onChangeValue('tc', !tc)} sx={{ '& .MuiSvgIcon-root': { fontSize: 30 }, mr: '5px', color: '#fff' }} />}
+        label="Agree to T&C"
       />
 
       <Box sx={{ display: 'flex', mt: 5 }}>
@@ -109,7 +132,7 @@ const Company = (props) => {
           fullWidth
           variant="contained"
           onClick={setQuestions}
-          disabled={!tc}
+          disabled={!canSubmit}
           type="submit"
         >
           Next
